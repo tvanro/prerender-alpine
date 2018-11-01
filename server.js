6d43cd37009285a2d58c3b72a41402125498f618
Keny LIEOU <keny.lieou@it-consultis.com>
2018-11-01 11:27:27 +0700

    Add package prerender-extension-blacklist

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const server = prerender({
   chromeLocation: '/usr/bin/chromium-browser'
 });
 
+server.use(require('prerender-extension-blacklist'));
 server.use(prerender.blacklist());
 server.use(prerender.httpHeaders());
 // server.use(prerender.removeScriptTags());
